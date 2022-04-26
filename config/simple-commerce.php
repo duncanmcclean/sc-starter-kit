225a779cf8399548480a411a7891bed5e59c5603
Duncan McClean <duncan@mcclean.co.uk>
2022-04-26 22:18:53 +0100

    wip

--- config/simple-commerce.php
+++ config/simple-commerce.php
@@ -95,7 +95,7 @@ return [
     'field_whitelist' => [
         'orders' => [
             'shipping_name', 'shipping_address_line1', 'shipping_address_line2', 'shipping_city', 'shipping_region',
-            'shipping_postal_code', 'shipping_country', 'use_shipping_address_for_billing', 'billing_name', 'billing_address',
+            'shipping_postal_code', 'shipping_country', 'use_shipping_address_for_billing', 'billing_name', 'billing_address_line1',
             'billing_address_line2', 'billing_city', 'billing_region', 'billing_postal_code', 'billing_country',
         ],
 
