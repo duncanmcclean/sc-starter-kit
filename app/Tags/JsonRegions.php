8a0b185ec0c66bb4096ac4890906a3ee83fbb241
Duncan McClean <duncan@mcclean.co.uk>
2022-04-26 21:53:07 +0100

    Update Regions namespace

--- app/Tags/JsonRegions.php
+++ app/Tags/JsonRegions.php
@@ -2,7 +2,7 @@
 
 namespace App\Tags;
 
-use DoubleThreeDigital\SimpleCommerce\Support\Regions;
+use DoubleThreeDigital\SimpleCommerce\Regions;
 use Statamic\Tags\Tags;
 
 class JsonRegions extends Tags
